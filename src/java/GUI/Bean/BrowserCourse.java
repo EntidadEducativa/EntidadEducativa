cbeb5fec6770cbb28735759eec381f4e8598978a
Daniel <Daniel@Daniel>
2015-09-27 14:47:22 -0500

    buscador de curso full (peticiones de samuel).

--- src/java/GUI/Bean/BrowserCourse.java
+++ src/java/GUI/Bean/BrowserCourse.java
@@ -12,6 +12,7 @@ import DataAccess.Entity.Course;
 import DataAccess.Entity.Student;
 import DataAccess.Entity.Teacher;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -34,17 +35,47 @@ public class BrowserCourse {
     public void setNameCour(Course nameCour) {
         this.nameCour = nameCour;
     }
-   // private List<Course> courseCollectionEst;
+    private List<Course> courseCollectionEst;
+    private List<Course> resultCourse;
     private String message;
     private String courseName;
     private String errorBrowser;
-    
-    
+    private boolean busqueda = true;
+    private int indexCourse = 0;
+
+    public int getIndexCourse() {
+        return indexCourse;
+    }
+
+    public void setIndexCourse(int indexCourse) {
+        this.indexCourse = indexCourse;
+    }
+
     public BrowserCourse() {
+        resultCourse = new ArrayList<>();
+        courseCollectionEst = new ArrayList<>();
         errorBrowser = null;
          
     }
+    
+    
+    
+    public List<Course> getResultCourse() {
+        return resultCourse;
+    }
 
+    public void setResultCourse(List<Course> resultCourse) {
+        this.resultCourse = resultCourse;
+    }
+
+    public boolean isBusqueda() {
+        return busqueda;
+    }
+
+    public void setBusqueda(boolean busqueda) {
+        this.busqueda = busqueda;
+    }
+    
      
     public String getCourseName() {
         return courseName;
@@ -71,20 +102,25 @@ public class BrowserCourse {
     }
 
     public void login() throws IOException {
-        UserManagement manageAccount = new UserManagement();
-        Course courDatabase = manageAccount.findCourse(courseName);
-        nameCour = courDatabase;
 
-        if(!isLoggedIn()){
-            message = "Curso no existe";
+        UserManagement manageAccount = new UserManagement();
+        CourseManagment courseManage = new CourseManagment();
+        setCourseCollectionEst(courseManage.keepAllCourses());
+        resultCourse.clear();
+        
+        for(Course x:courseCollectionEst){
+            if(x.getCourseName().contains(courseName)){
+                resultCourse.add(x);
+            }
         }
-        else{
+        
+        if(resultCourse.isEmpty()){
+            message = "Curso No encontrado";
+        }else{
             message = "Curso encontrado con Exito";
-            FacesContext.getCurrentInstance().getExternalContext().redirect("CourseDescription.xhtml");
+            FacesContext.getCurrentInstance().getExternalContext().redirect("listCourse.xhtml");
             errorBrowser = null;
         }
-      
-            
     }
 
     public boolean isLoggedIn() {
@@ -99,8 +135,7 @@ public class BrowserCourse {
     public void setCurrentStu(Course nameCour) {
         this.nameCour = nameCour;
 
-    }
- /*   
+    } 
     public List<Course> getCourseCollectionEst() {
         return courseCollectionEst;
     }
@@ -119,5 +154,16 @@ public class BrowserCourse {
         setCourseCollectionEst(courseManage.keepAllCourses());
         FacesContext.getCurrentInstance().getExternalContext().redirect("courses.xhtml");
     }
-*/
+    
+    public String converCourseName(int item){
+        return resultCourse.get(item).getCourseName();
+    }
+    
+    public void redirectList() throws IOException{
+        nameCour = resultCourse.get(indexCourse);
+        message = "Curso encontrado con Exito";
+        resultCourse.clear();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("CourseDescription.xhtml");
+        errorBrowser = null;  
+    }
 }
